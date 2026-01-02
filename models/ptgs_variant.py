d59a09d3895293044d7a055c0146a846c1a70c8a
MeeSay <huycqter@gmail.com>
2026-01-02 20:57:23 +0700

    update variant table

--- models/ptgs_variant.py
+++ models/ptgs_variant.py
@@ -6,4 +6,5 @@ class PtgVariant(models.Model):
 
     name = fields.Char(string='Variant', required=True)
     value = fields.Float(string='Value (x)', required=True)
-    
+    text_color = fields.Char(string='Text Color', required=False)
+    background_color = fields.Char(string='Background Color', required=False)
